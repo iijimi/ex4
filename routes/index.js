f188c4a2b5ed91ffe6463ec5a0f0ba8a1e5a6e10
Surarak Tawinwisan <yyjh0827@gmail.com>
2019-07-14 10:33:11 +0700

    ซิมซาราบิมซิมซิม

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/create', function(req, res, next) {
+  res.render('create');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 module.exports = router;
